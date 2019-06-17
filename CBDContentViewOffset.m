e82bf3e897ec8b75dbc6d2d6f7474c8164ef7ae2
Nepeta <ominousness@protonmail.com>
2019-06-17 23:06:02 +0200

    fix min/max values

--- CBDContentViewOffset.m
+++ CBDContentViewOffset.m
@@ -11,16 +11,16 @@
 	self.verticalOffsetSliderView = [[CBDSliderView alloc] initWithFrame:CGRectMake(0, 0, 0, 0)];
 	self.verticalOffsetSliderView.titleLabel.text = @"VERTICAL OFFSET";
 	[self.verticalOffsetSliderView.slider addTarget:self action:@selector(updateVerticalOffset:) forControlEvents:UIControlEventValueChanged];
-	self.verticalOffsetSliderView.slider.minimumValue = -150.0;
-	self.verticalOffsetSliderView.slider.maximumValue = 150.0;
+	self.verticalOffsetSliderView.slider.minimumValue = -300.0;
+	self.verticalOffsetSliderView.slider.maximumValue = 800.0;
 	self.verticalOffsetSliderView.slider.continuous = YES;
 	[self.stackView addArrangedSubview:self.verticalOffsetSliderView];
 
 	self.horizontalOffsetSliderView = [[CBDSliderView alloc] initWithFrame:CGRectMake(0, 0, 0, 0)];
 	self.horizontalOffsetSliderView.titleLabel.text = @"HORIZONTAL OFFSET";
 	[self.horizontalOffsetSliderView.slider addTarget:self action:@selector(updateHorizontalOffset:) forControlEvents:UIControlEventValueChanged];
-	self.horizontalOffsetSliderView.slider.minimumValue = -150.0;
-	self.horizontalOffsetSliderView.slider.maximumValue = 150.0;
+	self.horizontalOffsetSliderView.slider.minimumValue = -400.0;
+	self.horizontalOffsetSliderView.slider.maximumValue = 400.0;
 	self.horizontalOffsetSliderView.slider.continuous = YES;
 	[self.stackView addArrangedSubview:self.horizontalOffsetSliderView];
 
